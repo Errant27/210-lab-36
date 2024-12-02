c407a6461530f7689a60586bc5331d0b83d5daee
Errant27 <temporal.cerauno@gmail.com>
2024-12-01 17:54:27 -0800

    commit 1

--- 210-lab-36/main.cpp
+++ 210-lab-36/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  210-lab-36
-//
-//  Created by Neil Orton on 12/1/24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+   
     return 0;
 }
